01b5bef21b427b4f876933fd70f6749967d758a6
Your Name <you@example.com>
2020-01-05 13:19:46 +0530

    setup

--- public/india/javascripts/inCustMapjs.js
+++ public/india/javascripts/inCustMapjs.js
@@ -39,7 +39,7 @@ function initMap() {
         });
     var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 7,
-          center: {lat: 23.5659115, lng: 87.2727577},
+          //center: {lat: 23.5659115, lng: 87.2727577},
           mapTypeId: 'roadmap',
           disableDefaultUI: true
         });
