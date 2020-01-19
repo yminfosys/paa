9fe22d554a29c8d91427c03de64044c400568da8
Your Name <you@example.com>
2020-01-19 16:48:18 +0530

    kakaglsudd

--- routes/india.js
+++ routes/india.js
@@ -548,10 +548,10 @@ router.post('/drv/completeReg', function(req, res, next) {
   
      //////////Driver Incomming Call//////
      router.get('/call', function(req, res, next) {
-      const player = require('play-sound')();
-      player.play('./public/india/audio/car_horn.mp3', (err) => {
-        if (err) console.log(`Could not play sound: ${err}`);
-    });
+    //   const player = require('play-sound')();
+    //   player.play('./public/india/audio/car_horn.mp3', (err) => {
+    //     if (err) console.log(`Could not play sound: ${err}`);
+    // });
       res.render('india/inDriverIncommingCall',{YOUR_API_KEY:process.env.API_KEY})
     });
    console.log("gg")
