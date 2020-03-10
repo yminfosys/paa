70ca650bbaae79ebf4ccca284972799b451ec6e2
Your Name <you@example.com>
2020-03-10 10:00:57 +0530

    d125o91

--- public/india/javascripts/inPreDriverMapjs.js
+++ public/india/javascripts/inPreDriverMapjs.js
@@ -5,7 +5,7 @@ var wachID;
 function initMap() {   
     map = new google.maps.Map(document.getElementById('map'), {
           zoom: 7,
-         center: {lat: 23.5659115, lng: 87.2727577},
+         //center: {lat: 23.5659115, lng: 87.2727577},
           mapTypeId: 'roadmap',
           disableDefaultUI: true,
           map:map
@@ -28,6 +28,13 @@ function initMap() {
     function clearWachposition(){
       navigator.geolocation.clearWatch(wachID);
     }
+
+     /////////GPS location update driver tracking///////
+   function driverLocationUpdate(position){
+    $.post('/india/drv/driverLocatioUpdate',{lat:position.coords.latitude,lng:position.coords.longitude},function(data){
+      //console.log(data);
+    });
+   }
 
     ///////////Circle Marker/////////
   
