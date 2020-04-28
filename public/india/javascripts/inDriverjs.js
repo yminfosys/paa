b2d434c7d7a08f620b9a5aea01c1c2b55e3e4523
Your Name <you@example.com>
2020-04-28 18:13:20 +0530

    ac12789914298

--- public/india/javascripts/inDriverjs.js
+++ public/india/javascripts/inDriverjs.js
@@ -159,7 +159,7 @@ function loginprocess(){
 
         ///////Display List Accept Call Details /////
         socket.on('CallAcceptListDisplay', function (resp) {
-            alert(resp.bookingID)
+            alert(resp.bookingID);
             if(resp.pilotID==getCookie("pilotID")){
                 $.post('/india/drv/clineDetalls',{pilotID:resp.pilotID,CustID:resp.CustID,bookingID:resp.bookingID},function(data){
                     $("#pickDrop-Content").css({"display":"block"});
--- routes/india.js
+++ routes/india.js
@@ -745,44 +745,49 @@ router.post('/AcceptCallByDriver', function(req, res, next) {
   res.io.emit("DriverAccepeCall",{CustID:req.body.CustID,pilotID:req.body.pilotID});
   res.send("emitDriverAccepeCall")
  });
-
+ 
  router.post('/saveDriverCallAndBooking', function(req, res, next) { 
      ///////Create Bookinng////
      GenbookingID({},function(NewBookinid){
-      database.ride({
-        bookingID:NewBookinid.bookingID,   
-        CustID:req.body.CustID,
-        pilotID:req.body.pilotID,
-        DriverType:req.body.DriverType,
-        picupaddress:req.body.originAds,
-        picuklatlng: [req.body.originLat, req.body.originLng],    
-        dropaddress:req.body.distAds,     
-        droplatlng:[req.body.distLat, req.body.distLng],        
-        kmtravels:req.body.totalDistance,
-        totalamount:req.body.totalAmt,
-        paymentBy:req.body.payMode,           
-        callbookingStatus:"Accept",
-        driverBusy:"busy",        
-        preRideOTP:randamNumber(), 
-      }).save(function(err){
-        //////CUST data////
-      database.customer.findOneAndUpdate({CustID:req.body.CustID},{$set:{
-        orderStage:'accept',
-        bookingID:NewBookinid.bookingID
-      }},function(er,cust){
-        database.driverlocation.findOneAndUpdate({ pilotID:req.body.pilotID},{$set:{driverBusy:"busy"}},function(e, dd){
-           res.cookie("driverBusy", "busy",{maxAge: 30*24*60*60*1000 });
-          ////Requiest for Driver display Call Accept List/////
+       console.log("saveDriverCallAndBooking");
 
-          console.log("order generat",req.cookies.driverBusy)
-           res.io.emit("CallAcceptListDisplay",{ pilotID:req.body.pilotID,CustID:req.body.CustID,bookingID:NewBookinid.bookingID});
+      res.io.emit("CallAcceptListDisplay",{ pilotID:req.body.pilotID,CustID:req.body.CustID,bookingID:NewBookinid.bookingID});
+      console.log("Emit saveDriverCallAndBooking");
+      res.send("Order Grnerate sucellfully")
+      // database.ride({
+      //   bookingID:NewBookinid.bookingID,   
+      //   CustID:req.body.CustID,
+      //   pilotID:req.body.pilotID,
+      //   DriverType:req.body.DriverType,
+      //   picupaddress:req.body.originAds,
+      //   picuklatlng: [req.body.originLat, req.body.originLng],    
+      //   dropaddress:req.body.distAds,     
+      //   droplatlng:[req.body.distLat, req.body.distLng],        
+      //   kmtravels:req.body.totalDistance,
+      //   totalamount:req.body.totalAmt,
+      //   paymentBy:req.body.payMode,           
+      //   callbookingStatus:"Accept",
+      //   driverBusy:"busy",        
+      //   preRideOTP:randamNumber(), 
+      // }).save(function(err){
+      //   //////CUST data////
+      // database.customer.findOneAndUpdate({CustID:req.body.CustID},{$set:{
+      //   orderStage:'accept',
+      //   bookingID:NewBookinid.bookingID
+      // }},function(er,cust){
+      //   database.driverlocation.findOneAndUpdate({ pilotID:req.body.pilotID},{$set:{driverBusy:"busy"}},function(e, dd){
+      //      res.cookie("driverBusy", "busy",{maxAge: 30*24*60*60*1000 });
+      //     ////Requiest for Driver display Call Accept List/////
+
+      //     console.log("order generat",req.cookies.driverBusy)
+      //      res.io.emit("CallAcceptListDisplay",{ pilotID:req.body.pilotID,CustID:req.body.CustID,bookingID:NewBookinid.bookingID});
           
-           res.send("Order Grnerate sucellfully")
-        })
+      //      res.send("Order Grnerate sucellfully")
+      //   })
 
-      });
+      // });
         
-      })
+      // })
     });
   
  });
